19ebd26207e218fe09784bed6879d31ec921798c
covicDev <80823913+covicDev@users.noreply.github.com>
2022-06-28 10:46:20 +0200

    Initial push.

---
+++ _AutoSave.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public static class _AutoSave
+{
+    static _AutoSave()
+    {
+        EditorApplication.playModeStateChanged += _saveSceneWhenEntersPlayMode;
+    }
+
+    private static void _saveSceneWhenEntersPlayMode(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            Debug.Log("Auto-saving...");
+            EditorSceneManager.SaveOpenScenes();
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
